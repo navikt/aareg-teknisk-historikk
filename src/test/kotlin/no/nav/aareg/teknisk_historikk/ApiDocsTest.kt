54a2a3544f727a840e38abdf7ab3affa539cbaeb
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-01-26 13:37:18 +0100

    Fiks feilende api-dokumentasjon og tester
    Oppgradering av spring security låste unna swagger-dokumentasjon
    Dette fikser vi ved å oppgradere til ny springdoc-versjon, og ved
    å justere spring security. I tillegg lager vi et par enkle tester
    på om endepunktene kan nås eller ikke

--- src/test/kotlin/no/nav/aareg/teknisk_historikk/ApiDocsTest.kt
+++ src/test/kotlin/no/nav/aareg/teknisk_historikk/ApiDocsTest.kt
@@ -5,14 +5,30 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
+import org.springframework.test.annotation.DirtiesContext
 
-class SwaggerTest : AaregTekniskHistorikkTest() {
+@DirtiesContext
+class ApiDocsTest : AaregTekniskHistorikkTest() {
     @Autowired
     lateinit var testRestTemplate: TestRestTemplate
 
     @Test
+    fun `api-docs-endepunkt er tilgjengelig`() {
+        val resultat = testRestTemplate.getForEntity("/api-docs", String::class.java)
+
+        Assertions.assertEquals(HttpStatus.OK, resultat.statusCode)
+    }
+
+    @Test
     fun `swagger-endepunkt er tilgjengelig`() {
         val resultat = testRestTemplate.getForEntity("/swagger-ui/index.html", String::class.java)
+
+        Assertions.assertEquals(HttpStatus.OK, resultat.statusCode)
+    }
+
+    @Test
+    fun `spec-fil er tilgjengelig`() {
+        val resultat = testRestTemplate.getForEntity("/openapi-spec.yaml", String::class.java)
 
         Assertions.assertEquals(HttpStatus.OK, resultat.statusCode)
     }
