b585ee5feea658f0b54d00e63171d9b5246c17aa
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-03-24 14:59:09 +0100

    Legg til jackson kotlin-modul
    Med dette kan vi forenkle data-klassene våre

--- src/main/kotlin/no/nav/aareg/teknisk_historikk/aareg_services/contract/Bruksperiode.kt
+++ src/main/kotlin/no/nav/aareg/teknisk_historikk/aareg_services/contract/Bruksperiode.kt
@@ -3,8 +3,7 @@ package no.nav.aareg.teknisk_historikk.aareg_services.contract
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Bruksperiode(
-    @JsonProperty(value = "fom", required = true)
-    var fom: String,
-    @JsonProperty(value = "tom", required = false)
-    var tom: String?
+    val fom: String,
+    @JsonProperty(required = false)
+    val tom: String?
 )
--- src/main/kotlin/no/nav/aareg/teknisk_historikk/aareg_services/contract/Sporingsinformasjon.kt
+++ src/main/kotlin/no/nav/aareg/teknisk_historikk/aareg_services/contract/Sporingsinformasjon.kt
@@ -1,17 +1,12 @@
 package no.nav.aareg.teknisk_historikk.aareg_services.contract
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Sporingsinformasjon(
-    @JsonProperty(value = "opprettetTidspunkt", required = true)
-    var opprettetTidspunkt: LocalDateTime,
-    @JsonProperty(value = "endretTidspunkt", required = true)
-    var endretTidspunkt: LocalDateTime,
-    @JsonProperty(value = "opprettetKilde", required = true)
-    var opprettetKilde: String,
-    @JsonProperty(value = "endretKilde", required = true)
-    var endretKilde: String,
+    val opprettetTidspunkt: LocalDateTime,
+    val endretTidspunkt: LocalDateTime,
+    val opprettetKilde: String,
+    val endretKilde: String,
 )
